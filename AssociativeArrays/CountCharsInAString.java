688a9056a8780e788ff0374f062d2bcd558ebb76
Bozhidar Bozhkov <bozhidar.bozhkov@outlook.com>
2022-04-28 22:49:27 +0300

    new task added

---
+++ AssociativeArrays/CountCharsInAString.java
@@ -0,0 +1,25 @@
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class CountCharsInAString {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String[] text = scanner.nextLine().split(" ");
+        Map<Character, Integer> result = new LinkedHashMap<>();
+
+        for (String s : text) {
+            for (int i = 0; i < s.length(); i++) {
+                char symbol = s.charAt(i);
+                if (!result.containsKey(symbol)){
+                    result.put(symbol, 1);
+                } else {
+                    result.put(symbol, result.get(symbol) + 1);
+                }
+            }
+        }
+        result.forEach((key, value) -> System.out.printf("%c -> %d%n", key, value));
+    }
+
+}
